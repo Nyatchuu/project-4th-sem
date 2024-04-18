c937d92a00157f95cc3a4f6d2ebfaf734c7fae58
Roman Chitrakar <87511718+Nyatchuu@users.noreply.github.com>
2024-04-18 09:39:24 +0545

    add

--- page-components/header.php
+++ page-components/header.php
@@ -1,12 +1,12 @@
 <header>
     <nav>
-        <h2><a href="/project-4th-master/pages/index.php">Traveller</a></h2>
+        <h2><a href="/project-4th/pages/index.php">Traveller</a></h2>
         <ul>
-            <li><a href="/project-4th-master/pages/services.php">Services</a></li>
-            <li><a href="/project-4th-master/pages/about-us.php">About Us</a></li>
-            <li><a href="/project-4th-master/pages/packages.php">Packages</a></li>
-            <li><a href="/project-4th-master/pages/contact.php">Contact Us</a></li>
-            <li><a href="/project-4th-master/login_system/logout.php">logout</a></li>
+            <li><a href="/project-4th/pages/services.php">Services</a></li>
+            <li><a href="/project-4th/pages/about-us.php">About Us</a></li>
+            <li><a href="/project-4th/pages/packages.php">Packages</a></li>
+            <li><a href="/project-4th/pages/contact.php">Contact Us</a></li>
+            <li><a href="/project-4th/login_system/logout.php">logout</a></li>
         
         </ul>       
     </nav>
